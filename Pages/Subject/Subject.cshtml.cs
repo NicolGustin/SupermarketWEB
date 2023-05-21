4fd69599ec37be96c1da9d6b24b73a21b1411c3f
Nicol Gustin <nicol930519@hotmail.com>
2023-05-21 00:04:01 -0500

    autenticación y autorización

---
+++ Pages/Subject/Subject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupermarketWEB.Pages.Subject
+{
+    public class SubjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
